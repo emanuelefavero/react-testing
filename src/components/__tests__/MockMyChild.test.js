f5a9c932a4e41bfaa391e617d91035c26037afd8
emanuelefavero <emafaux89@gmail.com>
2023-01-05 12:49:13 +0100

    clean up MockMyChild.test.js

--- src/components/__tests__/MockMyChild.test.js
+++ src/components/__tests__/MockMyChild.test.js
@@ -2,16 +2,13 @@ import { render, screen } from '@testing-library/react'
 import MockMyChild from '../MockMyChild'
 
 // Mocking a component
-jest.mock('../ChildComponent', () => {
-  const MockChildComponent = () => (
-    <div data-testid='mock-child-component'></div>
-  )
-  return MockChildComponent
-})
+jest.mock('../ChildComponent', () => ({ mockChildComponentId }) => (
+  <div data-testid='mockChildComponentId' />
+))
 
 test('render the mock child component', () => {
   // Here MockMyChild is the Parent Component
   render(<MockMyChild />)
-  const mockChildComponent = screen.getByTestId('mock-child-component')
+  const mockChildComponent = screen.getByTestId('mockChildComponentId')
   expect(mockChildComponent).toBeInTheDocument()
 })
